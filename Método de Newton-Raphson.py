ffae00922b283e895872e7c086224e00bc4c4932
UP220937 <up220937@alumnos.upa.edu.mx>
2025-11-10 13:51:13 -0600

    clases

--- Método de Newton-Raphson.py
+++ Método de Newton-Raphson.py
@@ -6,3 +6,15 @@ import tkinter as tk
 from tkinter import filedialog, messagebox, ttk
 import os
 
+class QRCodeGenerator:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Generador de Códigos QR")
+        self.root.geometry("500x600")
+        self.setup_ui()
+    
+    def setup_ui(self):
+        # Frame principal
+        main_frame = ttk.Frame(self.root, padding="20")
+        main_frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+        
