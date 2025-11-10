14c5f8d57252f1acae4f59df6202b4e0c283750b
UP220937 <up220937@alumnos.upa.edu.mx>
2025-11-10 13:45:22 -0600

    imports

--- Método de Newton-Raphson.py
+++ Método de Newton-Raphson.py
@@ -1,1 +1,7 @@
-#hola
+import qrcode
+from qrcode.image.styledpil import StyledPilImage
+from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
+from qrcode.image.styles.colormasks import RadialGradiantColorMask
+import tkinter as tk
+from tkinter import filedialog, messagebox, ttk
+import os
