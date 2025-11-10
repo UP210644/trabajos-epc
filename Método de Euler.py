299b347d5606a68660f62e47d4041d1eb80f0afc
UP210644 <up210644@alumnos.upa.edu.mx>
2025-11-10 13:46:07 -0600

    Actualizacion del metodo de euler

--- Método de Euler.py
+++ Método de Euler.py
@@ -2,3 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def metodo_euler(f, x0, y0, h, x_final):
+    """
+    Resuelve una EDO de primer orden y' = f(x, y) usando el Método de Euler.
+
+    Parámetros:
+    f: La función (EDO) que define la pendiente, f(x, y).
+    x0: Condición inicial para x.
+    y0: Condición inicial para y (es decir, y(x0) = y0).
+    h: Tamaño del paso.
+    x_final: Valor final de x hasta donde se quiere la aproximación.
+
+    Retorna:
+    (list, list): Dos listas, una con los valores de x y otra con los
+                  valores aproximados de y.
+    """
+    
