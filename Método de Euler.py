07decebca2e4e3a775a47dcf00085b6add952db4
UP210644 <up210644@alumnos.upa.edu.mx>
2025-11-18 01:55:55 -0600

    Se cambian los comentarios

--- Método de Euler.py
+++ Método de Euler.py
@@ -4,3 +4,4 @@ import math  # Trae la "calculadora científica" (para seno, coseno, raíz, pi, 
 import numpy as np  # Trae una "calculadora avanzada" para listas (la usaremos para la gráfica)
 import matplotlib.pyplot as plt  # Trae el "equipo de dibujo" (para hacer la gráfica)
 import sys  # Trae el "interruptor de emergencia" (para poder parar el programa con Ctrl+C)
+
