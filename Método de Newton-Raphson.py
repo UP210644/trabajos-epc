dfb1742635b324eef13bdbbbabbc4616e6f8b624
UP220937 <up220937@alumnos.upa.edu.mx>
2025-11-17 20:31:02 -0600

    modificacion en las importaciones

--- Método de Newton-Raphson.py
+++ Método de Newton-Raphson.py
@@ -1,20 +1,6 @@
-import qrcode
-from qrcode.image.styledpil import StyledPilImage
-from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
-from qrcode.image.styles.colormasks import RadialGradiantColorMask
+import numpy as np
+import matplotlib.pyplot as plt
+import sympy as sp
+from sympy import symbols, diff, lambdify
 import tkinter as tk
-from tkinter import filedialog, messagebox, ttk
-import os
-
-class QRCodeGenerator:
-    def __init__(self):
-        self.root = tk.Tk()
-        self.root.title("Generador de Códigos QR")
-        self.root.geometry("500x600")
-        self.setup_ui()
-    
-    def setup_ui(self):
-        # Frame principal
-        main_frame = ttk.Frame(self.root, padding="20")
-        main_frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
-        
+from tkinter import ttk, messagebox
